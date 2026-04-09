ee0c68eb09bd3bf16781de8cc1430bc3d51c8fb1
tgcarrero-code <tg.carreror@alum.up.edu.pe>
2026-04-09 16:57:20 -0500

    Commit_1.py
    Importo y preparo el ambiente

---
+++ scraper.py
@@ -0,0 +1,9 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+from webdriver_manager.chrome import ChromeDriverManager
+import pandas as pd
+import unidecode
+import time
+import re
