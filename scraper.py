dc181e37b722abd3e719204bbe3e2e24645e3ee7
tgcarrero-code <tg.carreror@alum.up.edu.pe>
2026-04-09 16:58:43 -0500

    Commit_2.py
    Llamo al driver y agrego el link del cual sacaremos la información

--- scraper.py
+++ scraper.py
@@ -7,3 +7,10 @@ import pandas as pd
 import unidecode
 import time
 import re
+
+service = Service(ChromeDriverManager().install())
+
+driver = webdriver.Chrome(service=service)
+
+url_unmsm="https://admision.unmsm.edu.pe/Website20262/A/A.html"
+driver.get(url_unmsm)
