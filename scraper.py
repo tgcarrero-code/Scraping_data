206361e243c8789e2b2b2f8f3c7ced37189842a8
tgcarrero-code <tg.carreror@alum.up.edu.pe>
2026-04-09 16:59:26 -0500

    Commit_3.py
    Crear una lista de links de resultados de la postulación a todas las carreras

--- scraper.py
+++ scraper.py
@@ -14,3 +14,11 @@ driver = webdriver.Chrome(service=service)
 
 url_unmsm="https://admision.unmsm.edu.pe/Website20262/A/A.html"
 driver.get(url_unmsm)
+
+url_carreras=driver.find_elements(By.CSS_SELECTOR, "table tbody tr td a")
+
+links= []
+
+for url in url_carreras:
+    link=url.get_attribute("href")
+    links.append(link)
